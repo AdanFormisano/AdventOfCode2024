d1752e23af8b08710aee3e73cfb2b289d9fbf137
AdanFormisano <formisano.1813917@studenti.uniroma1.it>
2024-12-06 15:27:55 +0100

    Day 1 - Part 2 solution

--- Day 1/main.js
+++ Day 1/main.js
@@ -2,7 +2,9 @@ const fs = require('node:fs');
 
 const list_first = [];
 const list_second = [];
+const id_map = new Map();
 let total_diff = 0;
+let total_similarity = 0;
 
 try {
     const data = fs.readFileSync('input', 'utf-8');
@@ -12,11 +14,14 @@ try {
         const ids = lines[i].split('   ');
         list_first.push(Number(ids[0]));
         list_second.push(Number(ids[1]));
+
+        if (!id_map.has(ids[0])) {
+            id_map.set(Number(ids[0]), 0);
+        }
     }
 
-    while (list_first.length > 0 && list_second.length > 0) {
-        compareLists();
-    }
+    compareLists();
+    listsSimilarity();
 
     console.log(`Total difference: ${total_diff}`);
 } catch (err) {
@@ -25,14 +30,33 @@ try {
 
 // Compares the smallest element of each list and calculates the difference
 function compareLists() {
-    const min_first = Math.min(...list_first);
-    const min_second = Math.min(...list_second);
+    const tmp_list_first = list_first.slice();
+    const tmp_list_second = list_second.slice();
 
-    const diff = Math.abs(min_first - min_second);
-    total_diff += diff;
+    while (tmp_list_first.length > 0 && tmp_list_second.length > 0) {
+        const min_first = Math.min(...tmp_list_first);
+        const min_second = Math.min(...tmp_list_second);
 
-    // console.log(`Min 1: ${min_first}\t Min 2: ${min_second}\t Diff: ${diff}\t Total: ${total_diff}`);
+        const diff = Math.abs(min_first - min_second);
+        total_diff += diff;
 
-    list_first.splice(list_first.indexOf(min_first), 1);
-    list_second.splice(list_second.indexOf(min_second), 1);
+        // console.log(`Min 1: ${min_first}\t Min 2: ${min_second}\t Diff: ${diff}\t Total: ${total_diff}`);
+
+        tmp_list_first.splice(tmp_list_first.indexOf(min_first), 1);
+        tmp_list_second.splice(tmp_list_second.indexOf(min_second), 1);
+    }
+}
+
+function listsSimilarity() {
+    for (const id of list_second) {
+        if (id_map.has(id)) {
+            id_map.set(id, id_map.get(id) + 1);
+        }
+    }
+
+    for (const [key, value] of id_map.entries()) {
+        total_similarity += value * key;
+    }
+
+    console.log(`Total similarity: ${total_similarity}`);
 }
